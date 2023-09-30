58c898e5d31a60ab74a2ff4957b5e484fcbdf0e3
Amar Prajapati <51408969+amarprajapati571@users.noreply.github.com>
2023-09-30 14:15:03 +0530

    Remove unwanted code and fix API URL

--- src/Services/MessageSlice.js
+++ src/Services/MessageSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
+const URLAPI = "http://localhost:1111/";
 export const NLPServer = createApi({
   reducerPath: 'NLPServer',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1111/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: URLAPI }),
   endpoints: (builder) => ({
     getResponse: builder.mutation({
       query: (data) => ({
